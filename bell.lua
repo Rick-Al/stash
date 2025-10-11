a24323f1df950cceebdd7cf889e86c01a6b068c5
Rick-Al <62562394+Rick-Al@users.noreply.github.com>
2025-10-10 20:09:02 -0400

    Update bell.lua

--- bell.lua
+++ bell.lua
@@ -2,11 +2,48 @@ local speaker = peripheral.find("speaker")
 local relay = peripheral.find("redstone_relay")
 local inputSide = "top"
 
+local active = true
+
+print("Waiting for ghosts...")
+
 while true do
-  speaker.playNote("bell", 1, 1)
-  relay.setOutput("front", true)
-  sleep(0.1)
-  speaker.playNote("bell", 1, 12)
-  relay.setOutput("front", false)
-  sleep(0.1)
+  os.pullEvent("redstone")
+
+  if redstone.getInput(inputSide) then
+    active = not active
+
+    if active then
+      print("We got one!")
+      parallel.waitForAny(
+        function()
+          while active do
+            speaker.playNote("bell", 1, 1)
+            relay.setOutput("front", true)
+            sleep(0.1)
+            speaker.playNote("bell", 1, 12)
+            relay.setOutput("front", false)
+            sleep(0.1)
+          end
+        end,
+        function()
+          while active do
+            os.pullevent("redstone")
+            if redstone.getInput(inputSide)
+              active = false
+              print("Ghosts, busted.")
+            end
+          end
+        end
+      )
+    else
+      print("Ghosts, busted.")
+      relay.setOutput("front", false)
+    end
+
+    while redstone.getInput(inputSide) do
+      os.pullEvent("redstone")
+    end
+  end
 end
+
+  
