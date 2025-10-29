0c7a78b592f6059944997ecca21ccf9ae37d544c
Rick-Al <62562394+Rick-Al@users.noreply.github.com>
2025-10-28 22:21:42 -0400

    Add scrolling welcome message to monitor
    This script scrolls a welcome message across a monitor.

---
+++ scroll.lua
@@ -0,0 +1,22 @@
+-- pastebin 51u17dkr
+
+local mon = peripheral.find("monitor")
+local text = "Welcome to World Trade Center Station"
+local monWidth, monHeight = mon.getSize()
+local pos = monWidth
+ 
+mon.clear()
+mon.setTextScale(3)
+ 
+while true do
+    mon.clear()
+    mon.setCursorPos(pos, 1)
+    mon.write(text)
+    pos = pos - 1
+    
+    if pos + #text < 0 then
+        pos = monWidth
+    end
+    
+    sleep(0.2)
+end
